84d543c721e0d333a010ca7655cc5d9fc3e0b537
skim <rumex13@naver.com>
2023-07-11 02:02:47 +0900

    remove deploy and .env

--- next.config.js
+++ next.config.js
@@ -4,5 +4,7 @@ module.exports = {
   basePath: "/morethan-log",
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+    loader: 'akamai',
+    path: '',
   },
 }
