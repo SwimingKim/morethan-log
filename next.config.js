4586856497ccb69684a365d8322f83521bf1a606
skim <rumex13@naver.com>
2023-07-11 02:53:04 +0900

    css, favicon

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: false,
   output: "out",
-  basePath: "/morethan-log",
+  basePath: "/blog",
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
     loader: 'akamai',
