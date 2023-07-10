b988dfccbc334948473192b52fbaf2e8d09c1add
skim <rumex13@naver.com>
2023-07-11 00:27:39 +0900

    update deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   reactStrictMode: false,
+  output: "export",
+  basePath: "/morethan-log",
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
