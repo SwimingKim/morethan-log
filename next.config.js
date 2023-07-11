fa65f37f57a6b5a8ea0ace64c471259a0f5a3106
skim <rumex13@naver.com>
2023-07-11 18:29:11 +0900

    save site config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: false,
   output: "out",
-  basePath: "/blog",
+  basePath: "/morethan-log",
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
     loader: 'akamai',
